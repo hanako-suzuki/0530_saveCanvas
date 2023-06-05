2c5eb044ac628781b33fb55c34d98c32d4994f58
hanako-suzuki <100546035+hanako-suzuki@users.noreply.github.com>
2023-06-05 13:44:36 +0900

    test

--- index.js
+++ index.js
@@ -14,26 +14,59 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const saveButton = document.getElementById('saveButton');
 
-// get IP address
-const script = document.createElement('script');
-script.src = 'https://ipinfo.io?callback=callback';
-document.body.appendChild(script);
-document.body.removeChild(script);
-function callback(data) {
-    console.log(data.ip);
-    const ip = data.ip;
-    console.log('your ip:', ip);
+// // get IP address
+// const script = document.createElement('script');
+// script.src = 'https://ipinfo.io?callback=callback';
+// document.body.appendChild(script);
+// document.body.removeChild(script);
+// function callback(data) {
+//     console.log(data.ip);
+//     const ip = data.ip;
+//     console.log('your ip:', ip);
 
-    const drawImage = new Image();
-    drawImage.src = 'test_input.png'
-    drawImage.onload = function(){
-        ctx.drawImage(drawImage, 0, 0);
-    };
+//     const drawImage = new Image();
+//     drawImage.src = 'test_input.png'
+//     drawImage.onload = function(){
+//         ctx.drawImage(drawImage, 0, 0);
+//     };
 
-    saveButton.addEventListener('click', () => {
-        saveCanvas(canvas, String(Date.now()) + '.png');
-    });
+//     saveButton.addEventListener('click', () => {
+//         saveCanvas(canvas, String(Date.now()) + '.png');
+//     });
+// }
+
+// make cookie
+const cookies = document.cookie;
+const cookieArray = cookies.split(';');
+
+if(cookieArray.length==0){
+    // make cookie
+    const userId = generateUserId(); // 一意の識別子を生成する関数
+    // 有効期限を1日（24時間）に設定します
+    const maxAge = 60 * 60 * 24; // 1日（24時間）の秒数
+    document.cookie = `user_id=${userId}; max-age=${maxAge}; path=/`;
+
+}else{
+    for (let i = 0; i < cookieArray.length; i++) {
+        const cookie = cookieArray[i].trim();
+        if (cookie.startsWith('user_id=')) {
+            const userId = cookie.substring('user_id='.length);
+            // ユーザーを識別して必要な処理を行う
+            // ...
+            break;
+        }
+    }
 }
+
+const drawImage = new Image();
+drawImage.src = 'test_input.png'
+drawImage.onload = function(){
+    ctx.drawImage(drawImage, 0, 0);
+};
+
+saveButton.addEventListener('click', () => {
+    saveCanvas(canvas, String(Date.now()) + '.png');
+});
 
 
 
